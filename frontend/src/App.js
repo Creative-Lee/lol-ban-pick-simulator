273336253e36ec382b7aef143b8c700e8b46ebe0
Dohyeon Lee <dlehgus1110@naver.com>
2022-05-03 00:28:02 +0900

    main 컴포넌트 html 구도 잡는중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState , useEffect} from 'react';
+import {Routes, Route, Link} from 'react-router-dom'
 
-function App() {
+import { Main } from './components'
+
+import './scss/App.scss'
+import axios from 'axios';
+
+export default function App() {
+
+  const [recentVersion , setRecentVersion] = useState('')
+  const [championDataList, setChampionDataList] = useState({});
+  const [isMount, setIsMount] = useState(false);
+
+  const getRecentVersion = () => {
+    axios.get('https://ddragon.leagueoflegends.com/api/versions.json')
+    .then(result => {
+      const versionList = result.data 
+      console.log(versionList) 
+      setRecentVersion(versionList[0])  
+    })
+    .catch(e=> console.log(e))
+  }
+
+  const getChampionData = () => {
+    axios.get(`http://ddragon.leagueoflegends.com/cdn/${recentVersion}/data/ko_KR/champion.json`)
+    .then(result => {
+      let championData = result.data.data
+      console.log(championData)
+      setChampionDataList(championData)      
+    })
+    .catch(e=> console.log(e))
+  }
+
+
+  // const imgUrl = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championNameList[0]}_0.jpg`
+
+  useEffect(()=> {
+    getRecentVersion()    
+    setIsMount(true);
+  },[])
+  
+  useEffect(()=>{
+    if(isMount){
+      getChampionData()
+    }    
+  },[recentVersion])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Main recentVersion={recentVersion} championDataList={championDataList} />
+  </>
   );
 }
-
-export default App;
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,200 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default function Main({recentVersion, championDataList}) {
+
+
+  return (
+    <Container className='ban-pick-board'>
+      <Row className='board-top'>
+        <Col className="team1">
+          <div className='team1__title'>
+            team1
+          </div>
+          <div className='team1__logo'>
+            team1 logo
+          </div>
+        </Col>
+
+        <Col className="match-info">
+          <div className="date">match date</div>
+          <div className="round">round</div>
+        </Col>
+
+        <Col className="team2">
+          <div className='team2__logo'>
+            team2 logo
+          </div>
+          <div className='team2__title'>
+            team2
+          </div>
+        </Col>
+      </Row>
+
+      <Row className='board-middle'>
+        <Col className="team1__summoners">
+          <div className="summoner1">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+          <div className="summoner2">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+          <div className="summoner3">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+          <div className="summoner4">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+          <div className="summoner5">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+        </Col>
+
+        <Col className="todays-goal">
+          <h4>
+            title
+          </h4>
+          <div>
+            Today's Goal
+          </div>
+        </Col>
+
+        <Col className="team2__summoners">
+        <div className="summoner1">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+          <div className="summoner2">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+          <div className="summoner3">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+          <div className="summoner4">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+          <div className="summoner5">
+            <div className="champion">
+              champion1 img
+            </div>
+            <div className="spell">
+              <div>spell1</div>
+              <div>spell2</div>              
+            </div>
+            <div className="player">
+              player
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className='board-bottom'>
+        <Col>
+          <div>
+            team1 벤3마리
+          </div>
+          <div>
+            team1 벤2마리
+          </div>
+        </Col>
+        
+        <Col>
+          <div>
+            승리 패배
+          </div>          
+        </Col>
+
+        <Col>
+          <div>
+            team2 벤3마리
+          </div>
+          <div>
+            team2 벤2마리
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+
